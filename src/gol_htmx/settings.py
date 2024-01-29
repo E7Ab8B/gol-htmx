f53878ccb57ddd62fa1f5aa85f54915714c37f0f
E7Ab8B <140745017+E7Ab8B@users.noreply.github.com>
2024-01-29 03:17:19 +0100

    feat(settings): Read secret key from env

--- src/gol_htmx/settings.py
+++ src/gol_htmx/settings.py
@@ -1,15 +1,16 @@
 from pathlib import Path
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-aq9chwi@e7(93*@cr+=r)$d)d1#npp)8wemu40#97n2s1fg=79"
-
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="&q0a9*#lfis8l+pf2uab*f+h*dt!!3+enxmpu4ff$@8stkyyuw",
+)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
