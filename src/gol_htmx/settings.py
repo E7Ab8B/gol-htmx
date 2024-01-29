484bdd94663c52d52e7af7d8edb5376d916491d7
E7Ab8B <140745017+E7Ab8B@users.noreply.github.com>
2024-01-29 12:45:26 +0100

    feat(settings): Add django_extensions to installed apps

--- src/gol_htmx/settings.py
+++ src/gol_htmx/settings.py
@@ -17,9 +17,9 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-# Application definition
-
-INSTALLED_APPS = [
+# APPS
+# ------------------------------------------------------------------------------
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -27,6 +27,15 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+
+THIRD_PARTY_APPS = [
+    "django_extensions",
+]
+
+LOCAL_APPS = []
+
+# https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
