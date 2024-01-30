bb7fb531d17201e31c925d930e2c27421dc1f2c1
E7Ab8B <140745017+E7Ab8B@users.noreply.github.com>
2024-01-30 16:34:50 +0100

    feat(urls): Include gol urls to root url conf

--- src/gol_htmx/urls.py
+++ src/gol_htmx/urls.py
@@ -1,6 +1,3 @@
-from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+urlpatterns = [path("", include("apps.gol.urls"))]
