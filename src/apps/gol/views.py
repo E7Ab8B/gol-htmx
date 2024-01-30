e7a4cc0538b8a8b2518f92482e7ed361175b9ac0
E7Ab8B <140745017+E7Ab8B@users.noreply.github.com>
2024-01-30 17:20:20 +0100

    feat(views): Add view for updating grid

--- src/apps/gol/views.py
+++ src/apps/gol/views.py
@@ -1,9 +1,17 @@
 from __future__ import annotations
 
 import random
-from typing import Any
+import time
+from collections.abc import Iterable
+from typing import TYPE_CHECKING, Any
 
+from django.http import StreamingHttpResponse
+from django.template.loader import render_to_string
+from django.views import View
 from django.views.generic import TemplateView
+
+if TYPE_CHECKING:
+    from django.http import HttpRequest
 
 
 class GameView(TemplateView):
@@ -16,3 +24,20 @@ class GameView(TemplateView):
 
 
 game_view = GameView.as_view()
+
+
+class UpdateView(View):
+    async def get(self, request: HttpRequest) -> StreamingHttpResponse:
+        return StreamingHttpResponse(self.stream(request), content_type="text/event-stream")
+
+    @classmethod
+    def stream(cls, request: HttpRequest) -> Iterable:
+        while True:
+            grid = [[random.choice([True, False]) for _ in range(20)] for _ in range(20)]  # noqa: S311
+            rendered_grid = render_to_string("gol/game.html#grid", {"grid": grid}, request)
+            yield f"data: {rendered_grid.replace('\n', '')}"
+            yield "\n\n"
+            time.sleep(1)
+
+
+update_view = UpdateView.as_view()
