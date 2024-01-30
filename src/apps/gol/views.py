5de78f94c74055d0a7aa77833ac8d5746438eed5
E7Ab8B <140745017+E7Ab8B@users.noreply.github.com>
2024-01-30 16:59:02 +0100

    feat(game): Generate random generated grid

--- src/apps/gol/views.py
+++ src/apps/gol/views.py
@@ -1,10 +1,18 @@
 from __future__ import annotations
 
+import random
+from typing import Any
+
 from django.views.generic import TemplateView
 
 
 class GameView(TemplateView):
     template_name = "gol/game.html"
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["grid"] = [[random.choice([True, False]) for _ in range(20)] for _ in range(20)]  # noqa: S311
+        return context
 
 
 game_view = GameView.as_view()
