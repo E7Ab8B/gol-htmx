53c2a81f3c1f83d34b37b45a6f2f6cb512434ea4
E7Ab8B <140745017+E7Ab8B@users.noreply.github.com>
2024-01-30 17:22:59 +0100

    feat(urls): Add URL route for update grid state

--- src/apps/gol/urls.py
+++ src/apps/gol/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-from .views import game_view
+from .views import game_view, update_view
 
-urlpatterns = [path("", game_view, name="game")]
+urlpatterns = [
+    path("", game_view, name="game"),
+    path("update/", update_view, name="update"),
+]
