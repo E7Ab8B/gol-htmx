17017049ad7c5541e7f66810e11dc335134fe3b6
E7Ab8B <140745017+E7Ab8B@users.noreply.github.com>
2024-02-02 00:37:38 +0100

    tests(game): Add tests for underpopulation rule

---
+++ src/apps/gol/tests/test_rules.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Literal
+
+import pytest
+
+from apps.gol.game.cell import Cell
+from apps.gol.game.rules import underpopulation
+
+if TYPE_CHECKING:
+    type AliveNeighbors = int
+    type RuleResult = bool | None
+
+ALIVE_NEIGHBORS_RANGE = range(9)
+
+
+class TestUnderpopulationRule:
+    @staticmethod
+    @pytest.mark.parametrize(
+        ("alive_neighbors", "expected"),
+        [
+            (0, False),
+            (1, False),
+            (2, None),
+            (3, None),
+            (4, None),
+            (5, None),
+            (6, None),
+            (7, None),
+            (8, None),
+        ],
+    )
+    def test_alive_cell(alive_neighbors: int, expected: Literal[False] | None) -> None:
+        state = underpopulation(Cell(is_alive=True), alive_neighbors)
+
+        assert state is expected
+
+    @staticmethod
+    @pytest.mark.parametrize("alive_neighbors", ALIVE_NEIGHBORS_RANGE)
+    def test_dead_cell(alive_neighbors: int) -> None:
+        state = underpopulation(Cell(), alive_neighbors)
+
+        assert state is None
