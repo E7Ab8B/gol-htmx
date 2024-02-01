ed6deb4166b9e940b3c6ee597e8550f60d48581a
E7Ab8B <140745017+E7Ab8B@users.noreply.github.com>
2024-02-02 00:59:36 +0100

    tests(game): Add tests for survival rule

--- src/apps/gol/tests/test_rules.py
+++ src/apps/gol/tests/test_rules.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING, Literal
 import pytest
 
 from apps.gol.game.cell import Cell
-from apps.gol.game.rules import underpopulation
+from apps.gol.game.rules import survival, underpopulation
 
 if TYPE_CHECKING:
     type AliveNeighbors = int
@@ -39,5 +39,34 @@ class TestUnderpopulationRule:
     @pytest.mark.parametrize("alive_neighbors", ALIVE_NEIGHBORS_RANGE)
     def test_dead_cell(alive_neighbors: int) -> None:
         state = underpopulation(Cell(), alive_neighbors)
+
+        assert state is None
+
+
+class TestSurvivalRule:
+    @staticmethod
+    @pytest.mark.parametrize(
+        ("alive_neighbors", "expected"),
+        [
+            (0, None),
+            (1, None),
+            (2, True),
+            (3, True),
+            (4, None),
+            (5, None),
+            (6, None),
+            (7, None),
+            (8, None),
+        ],
+    )
+    def test_alive_cell(alive_neighbors: int, expected: Literal[True] | None) -> None:
+        state = survival(Cell(is_alive=True), alive_neighbors)
+
+        assert state is expected
+
+    @staticmethod
+    @pytest.mark.parametrize("alive_neighbors", ALIVE_NEIGHBORS_RANGE)
+    def test_dead_cell(alive_neighbors: int) -> None:
+        state = survival(Cell(), alive_neighbors)
 
         assert state is None
