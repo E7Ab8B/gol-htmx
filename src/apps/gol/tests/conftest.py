8e5a34cc6a24493abf4b8c4926ec9c64c5d10078
E7Ab8B <140745017+E7Ab8B@users.noreply.github.com>
2024-02-05 00:47:59 +0100

    tests(game): Add grid fixtures

--- src/apps/gol/tests/conftest.py
+++ src/apps/gol/tests/conftest.py
@@ -8,3 +8,23 @@ from apps.gol.game.grid import Grid
 @pytest.fixture()
 def empty_grid() -> Grid:
     return Grid(rows=3, columns=3)
+
+
+@pytest.fixture()
+def glider_grid() -> Grid:
+    grid = Grid(rows=10, columns=10)
+    grid.set_cell(0, 1, True)
+    grid.set_cell(1, 2, True)
+    grid.set_cell(2, 0, True)
+    grid.set_cell(2, 1, True)
+    grid.set_cell(2, 2, True)
+    return grid
+
+
+@pytest.fixture()
+def blinker_grid() -> Grid:
+    grid = Grid(rows=10, columns=10)
+    grid.set_cell(1, 0, True)
+    grid.set_cell(1, 1, True)
+    grid.set_cell(1, 2, True)
+    return grid
