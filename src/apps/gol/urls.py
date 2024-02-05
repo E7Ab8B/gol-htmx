07ac8a063f89334ea7926b0c7e32ec7d99ca4dec
E7Ab8B <140745017+E7Ab8B@users.noreply.github.com>
2024-02-05 19:47:41 +0100

    feat(urls): Add cell-update path

--- src/apps/gol/urls.py
+++ src/apps/gol/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import game_view, update_view
+from .views import cell_update_view, game_view, update_view
 
 urlpatterns = [
     path("", game_view, name="game"),
     path("update/", update_view, name="update"),
+    path("cell-update/", cell_update_view, name="cell-update"),
 ]
