c0877453d05f48e36b275906bb32490631a4a7a1
E7Ab8B <140745017+E7Ab8B@users.noreply.github.com>
2024-02-14 14:32:33 +0100

    feat(urls): Add start-game path

--- src/apps/gol/urls.py
+++ src/apps/gol/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import cell_update_view, game_view, update_view
+from .views import cell_update_view, game_view, start_game_view, update_view
 
 urlpatterns = [
     path("", game_view, name="game"),
+    path("start-game", start_game_view, name="start-game"),
     path("update/", update_view, name="update"),
     path("cell-update/", cell_update_view, name="cell-update"),
 ]
