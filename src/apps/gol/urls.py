423d1ab8d94963b519c15a9b9dbf7de3434b0f13
E7Ab8B <140745017+E7Ab8B@users.noreply.github.com>
2024-02-14 18:37:20 +0100

    feat(urls): Remove cell-update path

--- src/apps/gol/urls.py
+++ src/apps/gol/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import cell_update_view, game_view, start_game_view, update_view
+from .views import game_view, start_game_view, update_view
 
 urlpatterns = [
     path("", game_view, name="game"),
     path("start-game", start_game_view, name="start-game"),
     path("update/", update_view, name="update"),
-    path("cell-update/", cell_update_view, name="cell-update"),
 ]
